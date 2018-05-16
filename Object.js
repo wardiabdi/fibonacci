d844e302e2c4ec260ace814d59d22e137f9a943f
Wardi Abdi <wardiabdi3@gmail.com>
2018-05-16 13:45:05 +0100

    objects  populated

--- Object.js
+++ Object.js
@@ -1,31 +1,51 @@
 
-let team = {
-    "Id": 0012, 
-    "Name": "James",
-    "Surname": "John",
-    "Dept": "HR"
-};
+// let team = {
+//     "Id": 0012,
+//     "Name": "James",
+//     "Surname": "John",
+//     "Dept": "HR"
+// };
 
-// team.push{Surname:"Willaim"};
+// let person = {
+//     Id: "",
+//     name: "",
+//     lastname: "",
+//     age: null,
+//     qualifications: "",
+//     skillSet: ["JS", "HTML", "CSS"]
+//       Department: "",
+//     Experience: "",
+//     pes
 
-let person = {
-    Id: "",
-    name: "",
-    lastname: "",
-      age:null,
-      qualifications:"",
-        skillSet:["JS","HTML","CSS"]
-      Department:"",
-      Experience:"",
-  
-  
-  };
-  console.log(person);
-  
-  let skillsMatch = function(n){
-  
-  
-  
-  
-  
-  }
+// };
+
+
+let person = function createPerson(Id, Firstname, Lastname, Age, Title, Qualifications, Department, ExperienceYears) {
+
+    let Person = {
+        Id: Id,
+        Firstname: Firstname,
+        Lastname: Lastname,
+        Age: Age,
+        Title: Title,
+        Qualifications: Qualifications,
+        Department: Department,
+        ExperienceYears: ExperienceYears
+    };
+
+    return Person;
+}
+
+// let Person = (, "", "");
+
+let mPerson = person('10', 'Smith', 'Wardi');
+console.log(mPerson);
+
+console.log(mPerson.Firstname);
+console.log(mPerson['Lastname']);
+
+mPerson.Firstname = 'John';
+console.log(mPerson.Firstname);
+
+mPerson['Lastname'] = 'Ronaldo';
+console.log(mPerson['Lastname']);
