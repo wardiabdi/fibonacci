6ee2b76a42d3cdaf2f8239d6a4dc1ca045a33ff2
Wardi Abdi <wardiabdi3@gmail.com>
2018-05-23 19:49:24 +0100

    no change

--- truthtable.js
+++ truthtable.js
@@ -1,7 +1,7 @@
 
 
 
-let myArray1 = [];
+
 
 
 
@@ -9,12 +9,13 @@ let myArray1 = [];
 let myRandomNum = function (n) {
     let False = 0;
     let True = 1;
+    let newTruth=[];
     let myArray = ["a","b","c"];
-    for (i = 0; i < myArray.length; i++) {
-        myArray1.push(myRandomNum[i]);
+    for (i = (Math.pow)0; i < myArray.length; i++) {
+        newTruth.push(myRandomNum[i]);
           console.log(i);
     }
-    return myArray1;
+    return newTruth;
 
 }
-console.log(myRandomNum(myArray1));
+console.log(myRandomNum(newTruth));
