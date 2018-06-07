61f427671d371dc8ea3d15c5059122ff08032928
Wardi Abdi <wardiabdi3@gmail.com>
2018-06-07 17:43:16 +0100

    if statemen

--- truthtable.js
+++ truthtable.js
@@ -1,29 +1,47 @@
+let newTruth = [];
 
 
 
 
-
-
-
-// let p3 = p1 + p2;
 let myRandomNum = function (n) {
     let False = 0;
     let True = 1;
     let newTruth = [];
     let myArray = ["a", "b", "c"];
-    for (let i = (Math.pow(2 < myArray.length)-1); i = 0; i--) {
 
-        for (let j = (Math.pow(2, myArray.length) - 1); j >= 0; j--) {
-            row[j] = (i & Math.pow(2, j)) ? true : false
+    for (let i = 0; i < myArray.length; i++) {
+
+        if (a < b) {
+            return true;
+            newTrut.push(myRandomNum[i]);
         }
+        if (!(a == b)) {
+            return false;
+        }
+        if (a && b === c) {
+            return true;
 
-    }
-    return newTruth;
+        }
+    };
+
+    return n;
 
 }
-newFunction();
-}
 
-function newFunction() {
-    row[j] = (i & Math.pow(2, j)) ? true : false;
-}
+let myArray = myRandomNum();
+console.log(myRandomNum);
+
+// AND gate 
+// a b c 
+// 0 0 0
+// 0 1 0
+// 1 0 0
+// 1 1 1
+
+
+// OR gate
+// a b c
+// 0 0 0
+// 0 1 1
+// 1 0 1
+// 1 1 1
